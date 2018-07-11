4816e5e4ff4e7b0a484ce6a780cdf97f2b628b79
Charlie Nolan <nolancharlie2@gmail.com>
2018-07-11 17:49:19 +0000

    installed postgres and updated database.yml

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,4 +2,13 @@ class TodoController<ApplicationController
     def index
         
     end
+    def new
+        
+    end
+    def repetitive
+        
+    end
+    def once
+        
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get 'todo/index', to: 'todo#index'
+  get 'todo/creator/new', to: 'todo#creator#new'
+  get 'todo/creator/once', to: 'todo#creator#once'
+  get 'todo/creator/repetitive', to: 'todo#creator#repetitive'
 end
